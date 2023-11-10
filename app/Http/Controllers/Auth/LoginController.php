22939fd47af9c3e322dd34d6de6db8104b9ba725
eduardohpaludo <eduardopaludo@gmail.com>
2023-11-09 22:47:57 -0300

    Fix login and created exception

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,14 +2,25 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Exceptions\InvalidAuthenticationException;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Resources\User\UserResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     public function __invoke(LoginRequest $request)
     {
-        return 'oi';
+        $input = $request->validated();
+
+        if(!Auth::attempt($input)){
+            throw new InvalidAuthenticationException();
+        }
+
+        $request->session()->regenerate();
+
+        return new UserResource(auth()->user());
     }
 }
