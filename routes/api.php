cbc5b84f95aa702b0b44500c3e0ef8e928fb2ea7
eduardohpaludo <eduardopaludo@gmail.com>
2023-11-12 10:34:01 -0300

    Created meController

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Me\MeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,9 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:sanctum')->get('/me', [MeController::class, 'show']);
 
 Route::post('/login', LoginController::class);
-Route::post('/logout', LogoutController::class);
+Route::middleware('auth:sanctum')->post('/logout', LogoutController::class);
